c4e27c725eb2cb62d9e18a1f09ad7cf8b51989c7
Frederik Hermans <frederik.hermans@hotmail.com>
2018-11-10 17:59:38 +0100

    refa: small changes to user.js and index.js

--- __test__/test.js
+++ __test__/test.js
@@ -1,5 +1,40 @@
-const sum = require('../db');
+// const IncrementCallback = async (req,res,next) => {
+//   await userModule.incrementCredit(2);
+//   await userModule.incrementCredit(2);
+//   await userModule.incrementCredit(3);
+//   await userModule.decrementCredit(2);
+//   const result = await userModule.findAll();
+//   res.send(result);
+// }
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+// app.get('/Increment', IncrementCallback);
+
+// const findByIdCallback = async (req,res,next) => {
+  
+//   const result = await userModule.findUserById(2);
+//   res.send(result);
+// }
+
+
+// app.get('/UserByID', findByIdCallback);
+
+// const findByNameCallback = async (req,res,next) => {
+//   const result = await userModule.findUserByUsername("Fred");
+//   res.send(result);
+// }
+// app.get('/UserByName', findByNameCallback);
+
+// const updateByIdCallback = async (req,res,next) => {
+//   const update = {
+//     settings: {
+//       gender: "both",
+//       age: 80,
+//       feedbackGender: { male: true, female: true },
+//       feedbackAge: [true, false, true, false]
+//     }
+//   } 
+//   await userModule.update(2,update);
+//   const feedback = await userModule.findUserById(2);
+//   res.send(feedback);
+// }
+// app.get('/Update', updateByIdCallback);
--- modules/user.js
+++ modules/user.js
@@ -3,14 +3,10 @@ const db = require("../db");
 // Functionality coupled to images: Create Image, Vote on Image (View)
 
 // Sequelize models
-const Op = db.sequelize.Op;
-const Image = db.models.Image;
-const View = db.models.View;
 const User = db.models.User;
 
 // Upload an image for a user
 // Returns a promise
-// works
 const addUser = (username, password, settings) => {
   return User.create({
     username: username,
@@ -20,17 +16,14 @@ const addUser = (username, password, settings) => {
   });
 };
 
-// works
 const incrementCredit = (userId) => {
   return User.increment('credits', {where: {id: userId}})
 }
 
-// works
 const decrementCredit = (userId) => {
   return User.decrement('credits', {where: {id: userId}})
 }
 
-// Works
 const findUserById = (userId) => {
   return User.findOne({
     where: {
@@ -39,14 +32,12 @@ const findUserById = (userId) => {
   });
 }
 
-// Works
 const update = (userId, obj) => {
   return User.update(obj, {
     where: {id: userId}
   })
 }
 
-// Works
 const findUserByUsername = (username) => {
   return User.findOne({
     where: {
@@ -55,7 +46,6 @@ const findUserByUsername = (username) => {
   })
 }
 
-// Works
 const findAll = () => {
   return User.findAll()
 }
